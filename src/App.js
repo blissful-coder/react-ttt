9bf63fcf73d23d1a9640dfd82716475a91714709
blissful_coder <anurag.negi1729@gmail.com>
2024-10-13 20:22:31 +0530

    code(ci): initial setup and click and add X

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import {useState} from 'react';
+
+
+function Square() {
+  const [value, setvalue] = useState(null);
+
+  function handleClick() {
+    setvalue('X');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  )
+}
